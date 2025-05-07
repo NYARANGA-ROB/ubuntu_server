102e320b7921ced474bed426edb91e1777aa5a80
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2025-05-07 12:24:04 +0300

    Add files via upload

---
+++ data/educations.ts
@@ -0,0 +1,28 @@
+import _ from 'lodash';
+
+export const educations = [
+  {
+    id: _.uniqueId(),
+    timeLine: '2017 - 2021',
+    degree: 'Bachelor of Science in Computer Science and Engineering (CSE)',
+    school: 'American International University-Bangladesh (AIUB)',
+    location: 'Dhaka, Bangladesh',
+    gpa: '3.32/4.0',
+  },
+  {
+    id: _.uniqueId(),
+    timeLine: '2014 - 2016',
+    degree: 'Higher Secondary Certificate (HSC)',
+    school: 'Shah Makhdum College',
+    location: 'Rajshahi, Bangladesh',
+    gpa: '4.50/5.0',
+  },
+  {
+    id: _.uniqueId(),
+    timeLine: '2009 - 2013',
+    degree: 'Secondary School Certificate (SSC)',
+    school: 'Asian School & College',
+    location: 'Nachole, Chapai Nawabganj',
+    gpa: '4.25/5.0',
+  },
+];
